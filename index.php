a95c82e907b5fc6b963a33c11d406a3c25f0f1d4
Abhishek B <abmdn1242@gmail.com>
2024-01-10 21:04:10 +0530

    Landing page change

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                Restaurant POS
+                CANTEEN
             </div>
             <div class="links">
                 <a href="admin/">Admin Log In</a>
